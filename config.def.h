98e6c7ceaf5bfaad43cee310b4558e4f647c8f2d
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2022-03-09 12:10:27 +0100

    More colors for selected items on the sides.

--- config.def.h
+++ config.def.h
@@ -1,26 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
-/* -fn option overrides fonts[0]; default X11 font or font set */
+static int topbar = 1;          /* -b  option; if 0, dmenu appears at bottom */
+static int fuzzy = 1;           /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static int centered = 0;        /* -c option; centers dmenu on screen */
+static int min_width = 500;     /* minimum width when centered */
+                                /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
   "mononoki Nerd Font-20",
-	"monospace:size=10"
+  "monospace:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt = NULL;  /* -p  option; prompt to the left of input field */
 
 // THEME: Cobalt_Neon
 static const char *colors[SchemeLast][2] = {
-	                         /*    fg         bg   */
-	[SchemeSel]            = { "#142630", "#781aa0" },  // only selected item.
-	[SchemeMid]            = { "#8ff586", "#142838" },  // side selections
-	[SchemeNorm]           = { "#8ff586", "#142838" },  // general
-	[SchemeSelHighlight]   = { "#3aa5ff", "#142630" },  // filtered characters selection
-	[SchemeNormHighlight]  = { "#3aa5ff", "#142630" },  // filtered characters others
-	[SchemeOut]            = { "#142630", "#8ff586" },
+                           /*    fg         bg   */
+  [SchemeSel]            = { "#142630", "#8ff586" },  // the item under the cursor, unselected
+  [SchemeSelOut]         = { "#142630", "#781aa0" },  // the item under the cursor, selected
+  [SchemeMid]            = { "#8ff586", "#142838" },  // the items on the sides, unselected
+  [SchemeMidOut]         = { "#8ff586", "#084fb0" },  // the items on the sides, selected
+  [SchemeSelHighlight]   = { "#3aa5ff", "#142630" },  // filtered characters under the cursor
+  [SchemeNormHighlight]  = { "#3aa5ff", "#142630" },  // filtered characters on the sides
+  [SchemeNorm]           = { "#8ff586", "#142838" },  // anything else, unselected
+  [SchemeOut]            = { "#142630", "#8ff586" },  // anything else, selected
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
--- dmenu.c
+++ dmenu.c
@@ -30,7 +30,7 @@
 
 /* enums */
 enum { SchemeNorm, SchemeSel, SchemeNormHighlight, SchemeSelHighlight,
-       SchemeOut, SchemeMid, SchemeLast }; /* color schemes */
+       SchemeOut, SchemeMid, SchemeMidOut, SchemeSelOut, SchemeLast }; /* color schemes */
 
 
 struct item {
@@ -177,9 +177,15 @@ drawitem(struct item *item, int x, int y, int w)
 {
 	int r;
 	if (item == sel)
-		drw_setscheme(drw, scheme[SchemeSel]);
+		if (item->out)
+			drw_setscheme(drw, scheme[SchemeSelOut]);
+		else
+			drw_setscheme(drw, scheme[SchemeSel]);
 	else if (item->left == sel || item->right == sel)
-		drw_setscheme(drw, scheme[SchemeMid]);
+		if (item->out)
+			drw_setscheme(drw, scheme[SchemeMidOut]);
+		else
+			drw_setscheme(drw, scheme[SchemeMid]);
 	else if (item->out)
 		drw_setscheme(drw, scheme[SchemeOut]);
 	else
